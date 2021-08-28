fd239640c3d089791c043a284a584495eeee813e
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-08-27 22:33:04 -0400

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end
     render json: students
   end
 
+  def show
+    id = params[:id]
+    student = Student.find(id)
+    render json: student
+  end
 end
